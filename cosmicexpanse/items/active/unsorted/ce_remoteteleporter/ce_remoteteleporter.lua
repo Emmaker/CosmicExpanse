cffbd95dd64c1a691fe9bfae494e2860f4f9793c
JamesTheMaker <jamesthemaker2005@gmail.com>
2024-07-10 23:36:13 -0400

    Remote teleporter tweaks

--- cosmicexpanse/items/active/unsorted/ce_remoteteleporter/ce_remoteteleporter.lua
+++ cosmicexpanse/items/active/unsorted/ce_remoteteleporter/ce_remoteteleporter.lua
@@ -1,4 +1,33 @@
-function activate(fireMode, shiftHeld)
-    item.consume(1)
-    player.warp("OwnShip", "beam")
+function init()
+  self.swingTime = config.getParameter("swingTime")
+  activeItem.setArmAngle(-math.pi / 2)
+end
+
+function update(dt, fireMode, shiftHeld)
+  updateAim()
+
+  if not self.swingTimer and fireMode == "primary" and player then
+    self.swingTimer = self.swingTime
+  end
+
+  if self.swingTimer then
+    self.swingTimer = math.max(0, self.swingTimer - dt)
+
+    activeItem.setArmAngle((-math.pi / 2) * (self.swingTimer / self.swingTime))
+
+    if self.swingTimer == 0 then
+      teleport()
+    end
+  end
+end
+
+function teleport()
+  player.warp("OwnShip", "beam")
+
+  item.consume(1)
+end
+
+function updateAim()
+  self.aimAngle, self.aimDirection = activeItem.aimAngleAndDirection(0, activeItem.ownerAimPosition())
+  activeItem.setFacingDirection(self.aimDirection)
 end
