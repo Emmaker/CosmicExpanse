5600d57b5411a59a86017c240fc73cb43727a3e0
JamesTheMaker <jamesthemaker2005@gmail.com>
2024-07-10 20:45:41 -0400

    Add aiming to MM upgrades

--- cosmicexpanse/items/active/unsorted/ce_mmupgrader/ce_mmupgrader.lua
+++ cosmicexpanse/items/active/unsorted/ce_mmupgrader/ce_mmupgrader.lua
@@ -1,11 +1,39 @@
 require "/scripts/util.lua"
 
-function activate(fireMode, shiftHeld)
-  item.consume(1)
+function init()
+  self.upgrades = config.getParameter("upgrades")
+  self.swingTime = config.getParameter("swingTime")
+  activeItem.setArmAngle(-math.pi / 2)
+end
 
+function update(dt, fireMode, shiftHeld)
+  updateAim()
+
+  if not self.swingTimer and fireMode == "primary" and player then
+    self.swingTimer = self.swingTime
+  end
+
+  if self.swingTimer then
+    self.swingTimer = math.max(0, self.swingTimer - dt)
+
+    activeItem.setArmAngle((-math.pi / 2) * (self.swingTimer / self.swingTime))
+
+    if self.swingTimer == 0 then
+      upgradeMM()
+    end
+  end
+end
+
+function upgradeMM()
   local slot = config.getParameter("essentialSlot", "beamaxe")
-  local item = player.essentialItem(slot)
-  util.mergeTable(item.parameters, config.getParameter("upgrades"))
-  sb.logInfo("item: %s", item)
-  player.giveEssentialItem(slot, item)
+  local slotItem = player.essentialItem(slot)
+  util.mergeTable(slotItem.parameters, self.upgrades)
+  player.giveEssentialItem(slot, slotItem)
+
+  item.consume(1)
+end
+
+function updateAim()
+  self.aimAngle, self.aimDirection = activeItem.aimAngleAndDirection(0, activeItem.ownerAimPosition())
+  activeItem.setFacingDirection(self.aimDirection)
 end
